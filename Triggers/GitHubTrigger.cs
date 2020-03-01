356099b263ab250e4861da538acc67f883af8697
ujkkju117 <ujkkju117@outlook.com>
2020-03-01 17:13:20 +1030

    adding triggers.

---
+++ Triggers/GitHubTrigger.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+  
+namespace GitPipelines.Triggers
+{using GitPipelines.Interface;
+    class GitHubTrigger : ITrigger
+    {
+    }
+}
