ad4fd932685873302947da2207b3cd3242786ab3
Justin Branson <brajm008@mymail.unisa.edu.au>
2020-03-02 11:56:44 +1030

    Add Azure Testing as POC for pipeline matrix deployments.

---
+++ AzureTesting/AzureTesting.cs
@@ -0,0 +1,19 @@
+using GitPipelines;
+using NUnit.Framework;
+
+namespace AzureTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var testPipeline = new Pipeline();
+        }
+    }
+}
