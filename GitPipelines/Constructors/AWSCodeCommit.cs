46ddc8dd9e9691dc7adc5d7b9cf47c53e4df1199
ujkkju117 <ujkkju117@outlook.com>
2020-03-05 20:05:29 +1030

    add AWSCodeCommit as a future target.

---
+++ GitPipelines/Constructors/AWSCodeCommit.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+using GitPipelines.Interfaces;
+
+namespace GitPipelines.Constructors
+{
+    /// <summary>
+    /// Class that is used to define how Azure handles their pipelines.
+    /// </summary>
+    public class AWSCodeCommit : IPipeline
+    {
+        public AWSCodeCommit(Pipeline pipeline)
+        {
+        }
+
+        public Dictionary<string, IJob> Jobs { get; set; }
+    }
+}
--- GitPipelines/Pipeline.cs
+++ GitPipelines/Pipeline.cs
@@ -22,8 +22,10 @@ namespace GitPipelines
         }
 
         public AzurePipeline AzurePipeline() { return new AzurePipeline(this); }
+        public AWSCodeCommit AWSCodeCommit() { return new AWSCodeCommit(this); }
         public BitBucketPipeline BitBucketPipeline() { return new BitBucketPipeline(this); }
         public GitHubPipeline GitHubPipeline() { return new GitHubPipeline(this); }
         public GitLabPipeline GitLabPipeline() { return new GitLabPipeline(this); }
+       
     }
 }
